dbd4892d43454f48ef3366f5c5c59fe316142fc3
AndreMatioski02 <matioskiandre4@gmail.com>
2024-09-17 21:13:32 -0300

    Auth implementation

--- View/LoginView.swift
+++ View/LoginView.swift
@@ -1,48 +1,132 @@
 import SwiftUI
 
 struct LoginView: View {
-    @StateObject private var viewModel = AuthViewModel()
+    @StateObject private var authViewModel = AuthViewModel()
     @State private var path = NavigationPath()
-
+    
     var body: some View {
         NavigationStack(path: $path) {
-            VStack {
-                Text("Login")
-                    .font(.largeTitle)
-                    .padding(.bottom, 40)
+            ZStack {
+                LinearGradient(
+                    gradient: Gradient(colors: [
+                        Color(UIColor(red: 27/255, green: 0/255, blue: 71/255, alpha: 1)),
+                        Color(red: 99/255, green: 0/255, blue: 251/255)
+                    ]),
+                    startPoint: .top,
+                    endPoint: .bottom
+                )
+                .ignoresSafeArea()
                 
-                TextField("Email", text: $viewModel.email)
-                    .textFieldStyle(RoundedBorderTextFieldStyle())
-                    .padding(.horizontal)
-                
-                SecureField("Password", text: $viewModel.password)
-                    .textFieldStyle(RoundedBorderTextFieldStyle())
-                    .padding(.horizontal)
-                    .padding(.bottom, 20)
-                
-                if let errorMessage = viewModel.errorMessage {
-                    Text(errorMessage)
-                        .foregroundColor(.red)
+                VStack {
+                    Spacer()
+                    
+                    VStack(alignment: .center, spacing: 16) {
+                        VStack(alignment: .leading) {
+                            Text("Entre na sua conta")
+                                .font(.title)
+                                .fontWeight(.bold)
+                        }.frame(maxWidth: .infinity, alignment: .leading)
+                        
+                        VStack(alignment: .leading, spacing: 8) {
+                            Text("E-mail")
+                                .font(.headline)
+                                .foregroundColor(Color(red: 93/255, green: 93/255, blue: 93/255))
+                                .fontWeight(.regular)
+                                .padding([.leading], 4)
+                            TextField("Email", text: $authViewModel.email)
+                                .font(.custom("input-sm", size: 16))
+                                .padding(16)
+                                .background(
+                                    RoundedRectangle(cornerRadius: 8)
+                                        .stroke(Color(red: 220/255, green: 199/255, blue: 253/255), lineWidth: 3)
+                                )
+                        }
+                        
+                        VStack(alignment: .leading, spacing: 8) {
+                            HStack() {
+                                Text("Senha")
+                                    .font(.headline)
+                                    .foregroundColor(Color(red: 93/255, green: 93/255, blue: 93/255))
+                                    .fontWeight(.regular)
+                                    .padding([.leading], 4)
+                                Spacer()
+                                
+                                Text("Recuperar senha")
+                                    .font(.headline)
+                                    .foregroundColor(.blue)
+                                    .fontWeight(.regular)
+                                    .padding([.leading], 4)
+                            }
+                            
+                            SecureField("Password", text: $authViewModel.password)
+                                .font(.custom("input-sm", size: 16))
+                                .padding(16)
+                                .background(
+                                    RoundedRectangle(cornerRadius: 8)
+                                        .stroke(Color(red: 220/255, green: 199/255, blue: 253/255), lineWidth: 3)
+                                )
+                        }
+                        
+                        if (authViewModel.errorMessage != nil && authViewModel.isAuthenticating == false) {
+                            Text("E-mail ou senha incorretos")
+                                .foregroundColor(.red)
+                        }
+                        
+                        if authViewModel.isAuthenticating == true {
+                            ProgressView()
+                                .progressViewStyle(CircularProgressViewStyle())
+                                .padding()
+                                .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/, alignment: .center)
+                        }
+                        
+                        Button(action: authViewModel.loginUser) {
+                            Text("Entrar")
+                                .frame(minWidth: 0, maxWidth: .infinity)
+                                .padding()
+                                .foregroundColor(.white)
+                                .background(Color(red: 99/255, green: 0/255, blue: 251/255))
+                                .cornerRadius(8)
+                        }
+                        
+                        Button(action: authViewModel.loginUser) {
+                            HStack() {
+                                Image(systemName: "applelogo").foregroundColor(.white)
+                                Text("Continuar com Apple")
+                                
+                            }
+                            .frame(minWidth: 0, maxWidth: .infinity, alignment: .center)
+                            .padding()
+                            .foregroundColor(.white)
+                            .background(Color.black)
+                            .cornerRadius(8)
+                        }
+                        
+                        HStack() {
+                            Text("Não possui uma conta?")
+                                .font(.headline)
+                                .foregroundColor(Color(red: 93/255, green: 93/255, blue: 93/255))
+                                .fontWeight(.regular)
+                                .padding([.leading], 4)
+                            
+                            NavigationLink(destination: ContentView()) {
+                                Text("Cadastre-se")
+                                    .font(.headline)
+                                    .foregroundColor(.blue)
+                                    .fontWeight(.regular)
+                                    .padding([.leading], 4)
+                            }
+                        }
+                        
+                        
+                    }
+                    .padding([.top, .bottom], 32)
+                    .padding([.leading, .trailing ,.bottom], 16)
+                    .background(Color.white)
+                    .cornerRadius(30, corners: [.topLeft, .topRight])
+                    .shadow(radius: 10)
                 }
-                
-                Button(action: viewModel.loginUser) {
-                    Text("Login")
-                        .frame(minWidth: 0, maxWidth: .infinity)
-                        .padding()
-                        .foregroundColor(.white)
-                        .background(Color.blue)
-                        .cornerRadius(8)
-                        .padding(.horizontal)
-                }
-                
-    
-                NavigationLink("Go to Home", value: "home")
-                    .padding()
-                    .opacity(viewModel.isAuthenticated ? 1 : 0)
-            }
-            .padding()
-            .navigationDestination(for: String.self) { value in
-                if value == "home" {
+                .ignoresSafeArea()
+                .navigationDestination(isPresented: $authViewModel.isAuthenticated) {
                     ContentView()
                 }
             }
@@ -50,8 +134,22 @@ struct LoginView: View {
     }
 }
 
-
-
 #Preview {
     LoginView()
+}
+
+extension View {
+    func cornerRadius(_ radius: CGFloat, corners: UIRectCorner) -> some View {
+        clipShape(RoundedCorner(radius: radius, corners: corners))
+    }
+}
+
+struct RoundedCorner: Shape {
+    var radius: CGFloat = .infinity
+    var corners: UIRectCorner = .allCorners
+    
+    func path(in rect: CGRect) -> Path {
+        let path = UIBezierPath(roundedRect: rect, byRoundingCorners: corners, cornerRadii: CGSize(width: radius, height: radius))
+        return Path(path.cgPath)
+    }
 }
--- ViewModel/AuthViewModel.swift
+++ ViewModel/AuthViewModel.swift
@@ -4,34 +4,37 @@ import FirebaseAuth
 class AuthViewModel: ObservableObject {
     @Published var email: String = ""
     @Published var password: String = ""
+    @Published var rememberLogin = false
     @Published var isAuthenticated = false
+    @Published var isAuthenticating = false
     @Published var errorMessage: String?
     
     func loginUser() {
+        self.isAuthenticating = true
+        
         FirebaseAuthService.shared.login(email: email, password: password) { [weak self] result in
-            switch result {
-            case .success(let user):
-                DispatchQueue.main.async {
+            DispatchQueue.main.async {
+                switch result {
+                case .success(let user):
                     self?.isAuthenticated = true
-                    print("Logged in as: \(user.email ?? "")")
-                }
-            case .failure(let error):
-                DispatchQueue.main.async {
+                    self?.errorMessage = nil
+                    self?.email = ""
+                    self?.password = ""
+                case .failure(let error):
                     self?.errorMessage = error.localizedDescription
                 }
+                self?.isAuthenticating = false
             }
         }
     }
     
     func signOut() {
         FirebaseAuthService.shared.signOut { [weak self] result in
-            switch result {
-            case .success():
-                DispatchQueue.main.async {
+            DispatchQueue.main.async {
+                switch result {
+                case .success():
                     self?.isAuthenticated = false
-                }
-            case .failure(let error):
-                DispatchQueue.main.async {
+                case .failure(let error):
                     self?.errorMessage = error.localizedDescription
                 }
             }
